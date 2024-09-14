d38e351069df8b64c1d40ebcbaca501f878bfc6e
Алина Ашарабзянова <Tyuiu.AsharabzyanovaAR@outlook.com>
2024-09-14 20:44:17 +0500

    Task 1

---
+++ Tyuiu.AsharabzyanovaAR.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.AsharabzyanovaAR.Sprint0.Task1.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!");
+            string name = "Игорь";
+            int age = 45;
+            Console.WriteLine("Здравствуйте меня зовут " + name + " мне " + age + " лет ");
+            Console.ReadKey();
+
+        }
+    }
+}
